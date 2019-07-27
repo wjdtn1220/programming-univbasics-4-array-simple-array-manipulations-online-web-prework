af9289b950248c78fbb21f5a62af5dd344ac88a4
Jeong Park <wjdtn1220@gmail.com>
2019-07-27 02:17:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,3 +35,13 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer, element)
+  array.delete(integer, element)
+end
