8ef06148c540ae180f73e682039c9eeaaae45875
Jeong Park <wjdtn1220@gmail.com>
2019-07-27 02:18:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer, element)
-  array.delete(integer, element)
+def using_delete_at(array, integer)
+  array.delete(integer)
 end
